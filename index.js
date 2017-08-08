82156ed981d3a34182381a7bb507c9bd96eb983c
Tony <lxh0124@126.com>
2017-08-08 13:19:54 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const EventEmitter = require('events');
+const date = require('./date.js');
+
+date.format(new Date);
+
+
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+
+myEmitter.once('event', (a, b) => {
+  console.log('an event occurred!', this );
+});
+
+myEmitter.on('event', (a, b) => {
+  console.log('an event occurred2!', this );
+});
+
+
+process.on('uncaughtException', (err) => {
+  console.error('whoops! there was an error');
+});
+
+
+myEmitter.emit('event', 'a', 'b');
+console.log('max listener = ' + myEmitter.getMaxListeners());
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.on('connection', (stream) => {
+  console.log('someone connected!');
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
